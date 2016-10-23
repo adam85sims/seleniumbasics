b4082522e6089f9c7a7d8f8ccd0bbf5e3d91be6f
Adam Sims <adam85sims@gmail.com>
2016-10-23 21:07:01 +0100

    Create Navigate1.java

---
+++ 4-OpenBrowserNavigate/Navigate1.java
@@ -0,0 +1,19 @@
+package SeleniumBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class OpenBrowserMaximize {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+
+		driver.get("http://www.w3schools.com/");
+
+		driver.manage().window().maximize();
+
+		driver.findElement(By.xpath(".//*[@id='navbtn_tutorials']")).click();
+	}
+
+}
